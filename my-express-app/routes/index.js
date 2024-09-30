463a4e4e3b912fcb63c3657ea63d8b32e6c2ea3e
Agung R Ariza <agungrarz@mhs.mdp.ac.id>
2024-09-30 17:46:35 +0700

    halaman Contact
    submit form method post, menampilkan data dari hasil submit

--- my-express-app/routes/index.js
+++ my-express-app/routes/index.js
@@ -2,14 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   res.render('about', {
     title: 'About Us'
   });
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact', {
+    title: 'Contact Us'
+  });
+});
+
+router.post('/submit-contact', (req, res, next) => {
+  const { name } = req.body;
+  res.send(`
+    <h1>Thank You, ${name}</h1>
+    <p>Your message has been received. We will get back to you soon!</p>
+    <a href="/contact">Back to Contact</a>
+    `);
 });
 
 module.exports = router;
