2f9a9756d47f2e59a1f88e039c6318a201df1135
Agung Riski Ariza <agungrarz@mhs.mdp.ac.id>
2024-10-04 15:03:08 +0700

    template

--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('home/beranda', { title: 'Beranda' });
-  res.render('index', { title: 'Beranda' });
+  // res.render('index', { title: 'Beranda' });
+  res.render('layouts/main', { title: 'Beranda' });
 });
 
 router.get('/paketwisata', function(req, res, next) {
