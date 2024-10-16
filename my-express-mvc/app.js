2c688f88b50803aa71547826a58131e0e701d514
Agung R Ariza <agungrarz@mhs.mdp.ac.id>
2024-10-16 19:58:58 +0700

    connect MongoDB

--- my-express-mvc/app.js
+++ my-express-mvc/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// KONEKSI KE MONGODB
+require('./app_server/models/db');
+
 var indexRouter = require('./app_server/routes/index'); //tambahkan app_server
 var usersRouter = require('./app_server/routes/users'); //tambahkan app_server
 
